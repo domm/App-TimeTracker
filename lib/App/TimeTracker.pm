5d52d6bccc0e52e8a0694a3dad46e6a8a24d68af
domm@VIENNA.PM <domm@VIENNA.PM@e3bc34c7-6c8b-4c3e-8987-a454d55b637a>
2008-01-23 15:05:33 +0000

    r240@d610:  domm | 2008-01-23 16:07:49 +0100  bumped version
    git-svn-id: http://svn.useperl.at/members/domm/trunk/TimeTracker@49 e3bc34c7-6c8b-4c3e-8987-a454d55b637a

--- lib/App/TimeTracker.pm
+++ lib/App/TimeTracker.pm
@@ -3,7 +3,7 @@ package App::TimeTracker;
 use 5.010;
 use warnings;
 use strict;
-use version; our $VERSION = version->new('0.06');
+use version; our $VERSION = version->new('0.07');
 
 =head1 NAME
 
