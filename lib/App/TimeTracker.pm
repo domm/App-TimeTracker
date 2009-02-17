3b38bebed9c761707f3b7753d1a15e3d6ce30ef2
Thomas Klausner <domm@cpan.org>
2009-02-17 20:52:50 +0100

    bumped version to 0.21

--- lib/App/TimeTracker.pm
+++ lib/App/TimeTracker.pm
@@ -3,7 +3,7 @@ package App::TimeTracker;
 use 5.010;
 use warnings;
 use strict;
-use version; our $VERSION = version->new('0.20');
+use version; our $VERSION = version->new('0.21');
 
 =head1 NAME
 
