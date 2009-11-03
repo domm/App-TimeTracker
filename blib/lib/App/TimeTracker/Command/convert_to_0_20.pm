f0930c16e044b60b90e3626c065d9fd39d3fc80f
Klaus Ita <klaus@worstofall.com>
2009-11-03 19:08:36 +0100

    synced on Tue Nov  3 19:08:36 2009

---
+++ blib/lib/App/TimeTracker/Command/convert_to_0_20.pm
@@ -0,0 +1,106 @@
+package App::TimeTracker::Command::convert_to_0_20;
+use 5.010;
+use strict;
+use warnings;
+use App::TimeTracker -command;
+use base qw(App::TimeTracker);
+use DBI;
+use App::TimeTracker::Projects;
+use DateTime::Format::ISO8601;
+
+sub usage_desc {"%c covert_to_0_20"}
+
+sub run {
+    my ( $self, $opt, $args ) = @_;
+
+    my $basedir = $self->app->storage_location;
+
+    my $DBH
+        = DBI->connect( 'dbi:SQLite:dbname=' . $basedir . '/timetracker.db' );
+    $self->_projects( $DBH, $basedir );
+
+    my $sth
+        = $DBH->prepare(
+        "select task.id as id,start,stop,pr.name from task,project as pr where task.project=pr.id"
+        );    # AND start > '2009-01-01'");
+    $sth->execute;
+    while ( my $r = $sth->fetchrow_hashref ) {
+        my $sthtag
+            = $DBH->prepare(
+            "select tag.tag from tag,task_tag where task_tag.tag=tag.id AND task_tag.task=?"
+            );
+        $sthtag->execute( $r->{id} );
+        my @tags;
+        while ( my ($tag) = $sthtag->fetchrow_array ) {
+            push( @tags, $tag );
+        }
+
+        my $task = App::TimeTracker::Task->new( {
+                start   => _dt( $r->{start} ),
+                project => $r->{name},
+                stop    => _dt( $r->{stop} ),
+                tags    => join( ' ', @tags ),
+                basedir => $basedir,
+            }
+        )->write;
+    }
+}
+
+sub _dt {
+    my $date = shift;
+    $date =~ s/ /T/;
+    $date =~ s/-//g;
+    $date =~ s/://g;
+    my $dt = DateTime::Format::ISO8601->parse_datetime($date);
+    return $dt->epoch;
+}
+
+sub _projects {
+    my ( $self, $DBH, $basedir ) = @_;
+    my $projects = App::TimeTracker::Projects->read($basedir);
+
+    my $sth = $DBH->prepare("select name from project");
+    $sth->execute;
+    while ( my ($name) = $sth->fetchrow_array ) {
+        $name =~ s/\s+//g;
+        $projects->add($name);
+    }
+    $projects->write($basedir);
+}
+
+q{Listening to:
+    Arztwartezimmer
+};
+
+__END__
+
+=head1 NAME
+
+App::TimeTracker::Command::convert_to_0_20
+
+=head1 DESCRIPTION
+
+Converts an old (pre version 0.20) TimeTracker sqlite DB into the new 
+filebased format.
+
+=head1 METHODS
+
+=head3 usage_desc
+
+Usage Description for Getopt::Long::Descriptive
+
+=head3 run
+
+Implementation of command
+
+=head1 AUTHOR
+
+Thomas Klausner, C<< <domm at cpan.org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Thomas Klausner, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
---
+++ blib/lib/App/TimeTracker/Command/report.pm
@@ -0,0 +1,115 @@
+package App::TimeTracker::Command::report;
+use 5.010;
+use strict;
+use warnings;
+use App::TimeTracker -command;
+use base qw(App::TimeTracker);
+use DateTime;
+
+sub usage_desc {"report %o task"}
+
+sub opt_spec {
+    return ( shift->opt_spec_reports,
+        [ 'detail' => 'detailed report including tags' ] );
+}
+
+sub run {
+    my ( $self, $opt, $args ) = @_;
+
+    my $project = $opt->{project};
+    my $tag     = $opt->{tag};
+    my $tasks   = $self->find_tasks($opt);
+
+    my %report;
+    foreach my $file ( sort @$tasks ) {
+        my $task = App::TimeTracker::Task->read($file);
+
+        if ($tag) {
+            next unless $task->tags =~ /$tag/;
+        }
+
+        my $time
+            = (
+            $task->is_active ? $self->app->now->epoch : $task->stop->epoch )
+            - $task->start->epoch;
+        if ( $opt->{detail} ) {
+            $report{ $task->project }->[0] += $time;
+            foreach my $tag ( split( /\s+/, $task->tags ) ) {
+                $report{ $task->project }->[1]{$tag} += $time;
+            }
+        }
+        else {
+            $report{ $task->project } += $time;
+        }
+
+        say join( " ", $task->project, $task->start, $task->stop, $time )
+            if $opt->{verbose};
+    }
+
+    if ( $opt->{detail} ) {
+        while ( my ( $project, $data ) = each %report ) {
+            printf( "%- 20s %s\n",
+                $project,
+                App::TimeTracker::Task->beautify_seconds( $data->[0] ) );
+            while ( my ( $tag, $time ) = each %{ $data->[1] } ) {
+                printf( "   %- 20s %s\n",
+                    $tag, App::TimeTracker::Task->beautify_seconds($time) );
+            }
+        }
+    }
+    else {
+        while ( my ( $project, $time ) = each %report ) {
+            printf( "%- 20s %s\n",
+                $project, App::TimeTracker::Task->beautify_seconds($time) );
+        }
+    }
+
+}
+
+q{Listening to:
+    howling wind on Feuerkogel
+};
+
+__END__
+
+=head1 NAME
+
+App::TimeTracker::Command::report - generate a report of time spend on all tasks
+
+=head1 DESCRIPTION
+
+Implements the 'report' command, which generates a report of time spend on all tasks.
+
+  ~$ tracker report
+
+
+=head1 METHODS
+
+=head3 usage_desc
+
+Usage Description for Getopt::Long::Descriptive
+
+=head3 opt_spec
+
+Command line options definition
+
+=head3 validate_args
+
+Command line options validation
+
+=head3 run
+
+Implementation of command
+
+=head1 AUTHOR
+
+Thomas Klausner, C<< <domm at cpan.org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Thomas Klausner, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
