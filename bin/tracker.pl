c12d40d7777b2646674bdfcd8dfac6b478c4a01b
Thomas Klausner <domm@plix.at>
2010-07-12 01:38:05 +0200

    integrated MooseX::SimpleConfig & Getopt

---
+++ bin/tracker.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use App::TimeTracker;
+
+my $app = App::TimeTracker->new_with_options->run;
+
--- lib/App/TimeTracker.pm
+++ lib/App/TimeTracker.pm
@@ -3,6 +3,40 @@ use strict;
 use warnings;
 use 5.010;
 
+use File::HomeDir;
+use Moose;
+use MooseX::Types::Path::Class;
+with 'MooseX::SimpleConfig';
+with 'MooseX::Getopt';
+
+
+has 'home' => (
+    is=>'ro',
+    isa=>'Path::Class::Dir',
+    coerce=>1,
+    lazy_build=>1,
+);
+
+sub _build_home {
+    my $self = shift;
+    my $home = Path::Class::Dir->new(File::HomeDir->my_home,'.TimeTracker2');
+    warn $home;
+    $home->mkpath unless -d $home;
+    return $home;
+}
+
+has '+configfile' => (
+    default=>sub { Path::Class::File->new(File::HomeDir->my_home,'.TimeTracker2','tracker.ini') }
+);
+
+
+
+has 'foo'=>(isa=>'Str',is=>'rw',default=>'default');
+
+sub run {
+    my $self = shift;
+
+}
 
 
 1;
