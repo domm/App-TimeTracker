bccfa47d312761f7be87f9ec4ac3efcbe6e5eb24
Thomas Klausner <domm@plix.at>
2011-02-23 22:55:43 +0100

    add sync_via_git command

---
+++ lib/App/TimeTracker/Command/SyncViaGit.pm
@@ -0,0 +1,32 @@
+package App::TimeTracker::Command::SyncViaGit;
+use strict;
+use warnings;
+use 5.010;
+
+use Moose::Role;
+use Git::Repository;
+
+sub cmd_sync {
+    my $self = shift;
+
+    my $r = Git::Repository->new( work_tree => $self->home );
+
+    my @new = $r->run('ls-files' =>'-om');
+    foreach my $changed (@new) {
+        $r->run(add=>$changed);
+    }
+    
+    $r->run(commit => '-m','synced on '.$self->now);
+
+    foreach my $cmd (qw(pull push)) {
+        my $c = $r->command( $cmd );
+        print $c->stderr->getlines;
+        $c->close;
+    }
+}
+
+no Moose::Role;
+1;
+
+__END__
+
