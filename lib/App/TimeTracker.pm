0940bd38404d3a16f7e07561fc305056f85ceef4
Thomas Klausner <domm@plix.at>
2011-03-01 16:51:21 +0100

    updated version to 2.001

--- lib/App/TimeTracker.pm
+++ lib/App/TimeTracker.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use 5.010;
 
-our $VERSION = "1.001";
+our $VERSION = "2.001";
 # ABSTRACT: Track time spend on projects from the commandline
 
 use App::TimeTracker::Data::Project;
