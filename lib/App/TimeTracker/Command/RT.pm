ce35b341e7f2e253f71dedc0f8252dd72bcb5ccc
Thomas Klausner <domm@plix.at>
2011-03-01 20:43:13 +0100

    added RT prototype

---
+++ lib/App/TimeTracker/Command/RT.pm
@@ -0,0 +1,24 @@
+package App::TimeTracker::Command::RT;
+use strict;
+use warnings;
+use 5.010;
+
+use Moose::Role;
+
+has 'rt' => (is=>'ro',isa=>'Int');
+
+before 'cmd_start' => sub {
+    my $self = shift;
+
+    $self->insert_tag('RT'.$self->rt);
+
+    if ($self->meta->does_role('App::TimeTracker::Command::Git')) {
+        $self->branch('RT'.$self->rt) unless $self->branch;
+    }
+};
+
+no Moose::Role;
+1;
+
+__END__
+
