2aba4623444be162b0491ac73de616a4024b2741
Thomas Klausner <domm@plix.at>
2011-03-01 17:41:48 +0100

    some git plugin fiddling

--- lib/App/TimeTracker/Command/Git.pm
+++ lib/App/TimeTracker/Command/Git.pm
@@ -4,20 +4,48 @@ use warnings;
 use 5.010;
 
 use Moose::Role;
+use Git::Repository;
 
-#around 'cmd_stop' => sub {
-#    my $orig = shift;
-#    my $self = shift;
-#    $self->$orig(@_);
-#};
+has 'branch' => (is=>'ro',isa=>'Str');
+has 'merge' => (is=>'ro',isa=>'Bool');
 
-sub cmd_git {
+after 'cmd_start' => sub {
     my $self = shift;
-    say "git!!";
-}
 
-#after 'cmd_start' => sub {
-#};
+    return unless $self->branch;
+
+    my $r = Git::Repository->new( work_tree => '.' );
+    my $branch = $self->branch;
+    my %branches = map { s/^\s+//; $_=>1 } $r->run('branch');
+
+    if ($branches{'* '.$branch}) {
+        say "Already on branch $branch";
+        return;
+    }
+
+    if (!$branches{$branch}) {
+        $r->command('branch',$branch);
+    }
+
+    print $r->command('checkout',$branch)->stderr->getlines;
+};
+
+after cmd_stop => sub {
+    my $self = shift;
+    return unless $self->merge;
+
+    my $r = Git::Repository->new( work_tree => '.' );
+    my $branch = $self->branch;
+    my %branches = map { s/^\s+//; $_=>1 } $r->run('branch');
+
+    unless ($branches{'* '.$branch}) {
+        say "Not in branch $branch, won't merge.";
+        return;
+    }
+    my $tags = join(', ',map { $_->name } @{$self->tags}) || '';
+    $r->command('checkout','master');
+    $r->command("merge",$branch,"--no-ff",'-m',"implemented $branch $tags");
+};
 
 no Moose::Role;
 1;
