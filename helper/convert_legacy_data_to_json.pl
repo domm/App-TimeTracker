2d83f4c2976b07b82ef24f50e057d3dce65a1177
Klaus Ita <klaus@worstofall.com>
2011-03-01 14:17:12 +0100

    helper reports error files, also reports a result

--- helper/convert_legacy_data_to_json.pl
+++ helper/convert_legacy_data_to_json.pl
@@ -9,6 +9,7 @@ use Data::Dumper;
 use App::TimeTracker::Data::Task;
 use DateTime;
 use Path::Class;
+use Try::Tiny;
 
 $|=1;
 my ($in, $out) = @ARGV;
@@ -21,28 +22,36 @@ my @files = File::Find::Rule->file()->name(qr/\.(done|current)$/)
 
 foreach my $old (@files) {
     open( my $fh, "<", $old ) || die "Cannot read file $old: $!";
-    my %data;
-    while ( my $line = <$fh> ) {
-        chomp($line);
-        next unless $line =~ /^(\w+): (.*)/;
-        $data{$1} = $2;
-    }
-    
-    my @tags;
-    if ($data{tags}) {
-        foreach (split(/\s+/,$data{tags})) {
-            push(@tags,App::TimeTracker::Data::Tag->new({name=>$_}));
-        }
-    }
 
-    my $task = App::TimeTracker::Data::Task->new({
-        start=>DateTime->from_epoch(epoch=>$data{start}, time_zone=>'local'),
-        stop=>DateTime->from_epoch(epoch=>$data{stop}, time_zone=>'local'),
-        project=>App::TimeTracker::Data::Project->new({name=>$data{project}}),
-        tags=>\@tags,
-    });
-   
-    $task->save($out);
-    print '.';
+    try {
+
+        my %data;
+        while ( my $line = <$fh> ) {
+            chomp($line);
+            next unless $line =~ /^(\w+): (.*)/;
+            $data{$1} = $2;
+        }
+
+        my @tags;
+        if ($data{tags}) {
+            foreach (split(/\s+/,$data{tags})) {
+                push(@tags,App::TimeTracker::Data::Tag->new({name=>$_}));
+            }
+        }
+
+        my $task = App::TimeTracker::Data::Task->new({
+                start=>DateTime->from_epoch(epoch=>$data{start}, time_zone=>'local'),
+                stop=>DateTime->from_epoch(epoch=>$data{stop}, time_zone=>'local'),
+                project=>App::TimeTracker::Data::Project->new({name=>$data{project}}),
+                tags=>\@tags,
+        });
+
+        $task->save($out);
+        print '.';
+    }
+    catch {
+        die "An error occurred converting $old";
+    }
 }
 
+say "Finished processing " . scalar @files . " historic Tasks";
