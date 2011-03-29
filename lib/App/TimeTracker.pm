c323d6f28096ec10e4816ac4a8d48abb3f46a09d
Thomas Klausner <domm@plix.at>
2011-03-29 12:02:43 +0200

    release 2.004

--- lib/App/TimeTracker.pm
+++ lib/App/TimeTracker.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use 5.010;
 
-our $VERSION = "2.003";
+our $VERSION = "2.004";
 # ABSTRACT: Track time spend on projects from the commandline
 
 use App::TimeTracker::Data::Task;
