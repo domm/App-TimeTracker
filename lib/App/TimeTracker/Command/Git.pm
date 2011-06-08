6d5210067027b63a60ab71787ca4cffb289153aa
Thomas Klausner <domm@plix.at>
2011-06-08 22:13:59 +0200

    removed cmd_merge (git), because it was a bad idea

--- lib/App/TimeTracker/Command/Git.pm
+++ lib/App/TimeTracker/Command/Git.pm
@@ -51,23 +51,6 @@ after cmd_stop => sub {
     $r->command("merge",$branch,"--no-ff",'-m',"implemented $branch $tags");
 };
 
-sub cmd_merge {
-    my $self = shift;
-    
-    my $r = Git::Repository->new( work_tree => '.' );
-    my $branch = $self->branch;
-    my %branches = map { s/^\s+//; $_=>1 } $r->run('branch');
-
-    unless ($branches{'* '.$branch}) {
-        say "Not in branch $branch, won't merge.";
-        return;
-    }
-    my $tags = join(', ',map { $_->name } @{$self->tags}) || '';
-    $r->command('checkout','master');
-    $r->command("merge",$branch,"--no-ff",'-m',"implemented $branch $tags");
-
-}
-
 no Moose::Role;
 1;
 
