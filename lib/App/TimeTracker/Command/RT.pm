9ace98d55a42e5a08ad20e8f3c7537d567144836
Maros Kollar <maros@k-1.com>
2011-06-09 13:33:36 +0200

    Refactor/Improve RT plugin: Ascii-only branch names, add RT subject, use RT::Client::REST::Ticket

--- lib/App/TimeTracker/Command/RT.pm
+++ lib/App/TimeTracker/Command/RT.pm
@@ -7,10 +7,33 @@ use 5.010;
 
 use Moose::Role;
 use RT::Client::REST;
+use RT::Client::REST::Ticket;
 use Try::Tiny;
+use Unicode::Normalize;
 
-has 'rt' => (is=>'ro',isa=>'TT::RT',coerce=>1,documentation=>'RT: Ticket number');
+has 'rt' => (is=>'rw',isa=>'TT::RT',coerce=>1,documentation=>'RT: Ticket number', predicate => 'has_rt');
 has 'rt_client' => (is=>'ro',isa=>'RT::Client::REST',lazy_build=>1);
+has 'rt_ticket' => (is=>'ro',isa=>'RT::Client::REST::Ticket',lazy_build=>1);
+
+sub _build_rt_ticket {
+    my ($self) = @_;
+    
+    my $task = $self->_current_task;
+
+    if (defined $task
+        && defined $task->rt_id
+        && ! $self->has_rt) {
+        $self->rt($task->rt_id);
+    }
+
+    my $rt_ticket = RT::Client::REST::Ticket->new(
+        rt  => $self->rt_client,
+        id  => $self->rt,
+    );
+    $rt_ticket->retrieve;
+    return $rt_ticket;
+}
+
 sub _build_rt_client {
     my $self = shift;
     my $config = $self->config->{rt};
@@ -29,28 +52,36 @@ sub _build_rt_client {
     return $client;
 }
 
-before 'cmd_start' => sub {
+before ['cmd_start','cmd_continue'] => sub {
     my $self = shift;
-    return unless my $rt = $self->rt;
-    my $ticketname='RT'.$rt;
+    
+    return
+        unless $self->has_rt;
+    
+    my $ticketname='RT'.$self->rt;
 
     $self->insert_tag($ticketname);
+    $self->add_tag('RT: '.$self->rt_ticket->subject);
 
     if ($self->meta->does_role('App::TimeTracker::Command::Git')) {
-        my $ticket=$self->rt_client->show(type => 'ticket', id => $rt);
-        my $subject = $ticket->{Subject};
+        my $ticket= $self->rt_ticket;
+        my $subject = $ticket->subject;
+        $subject = NFKD($subject);
+        $subject =~ s/\p{NonspacingMark}//g;
         $subject=~s/\W/_/g;
         $subject=~s/_+/_/g;
         $subject=~s/^_//;
         $subject=~s/_$//;
-
-        $self->branch($ticketname.'_'.$subject) unless $self->branch;
+        $self->branch($ticketname.'_'.$subject) 
+            unless $self->branch;
     }
 };
 
 after 'cmd_start' => sub {
     my $self = shift;
-    return unless $self->config->{rt}{set_owner_to};
+    
+    return 
+        unless $self->config->{rt}{set_owner_to};
 
     my $task = $self->_current_task;
     return unless $task;
@@ -61,10 +92,9 @@ after 'cmd_start' => sub {
     }
 
     try {
-        $self->rt_client->edit( type => 'ticket', id => $ticket_id, set=>{
-            Status=>'open',
-            Owner=>$self->config->{rt}{set_owner_to},
-        });
+        $self->rt_ticket->owner($self->config->{rt}{set_owner_to});
+        $self->rt_ticket->status('open');
+        $self->rt_ticket->store();
     }
     catch {
         say $_;    
@@ -74,34 +104,45 @@ after 'cmd_start' => sub {
 after 'cmd_stop' => sub {
     my $self = shift;
 
-    return unless $self->config->{rt}{update_time_worked};
+    return 
+        unless $self->config->{rt}{update_time_worked};
 
-    my $task = $self->_previous_task;
-    return unless $task;
+    my $task = $self->_current_task;
+
+    return 
+        unless $task && $task->rounded_minutes > 0;
+
     my $ticket_id = $task->rt_id;
     unless ($ticket_id) {
         say "No RT ticket id found, cannot update TimeWorked";
         return;
     }
-
-    my $old = $self->rt_client->show(type => 'ticket', id => $ticket_id);
-    unless ($old) {
+    
+    unless ($self->rt_ticket) {
         say "Cannot find ticket $ticket_id in RT";
         return;
     }
-
-    my $worked = $old->{TimeWorked} || 0;
+    
+    
+    my $worked = $self->rt_ticket->time_worked || 0;
     $worked =~s/\D//g;
-
-    $self->rt_client->edit( type => 'ticket', id => $ticket_id, set=>{
-        TimeWorked=> $worked + $task->rounded_minutes
-    });
+    
+    $self->rt_ticket->time_worked( $worked + $task->rounded_minutes );
+    $self->rt_ticket->store;
 };
 
 sub App::TimeTracker::Data::Task::rt_id {
     my $self = shift;
     foreach my $tag (@{$self->tags}) {
         next unless $tag =~ /^RT(\d+)/;
+        return $1;
+    }
+}
+
+sub App::TimeTracker::Data::Task::rt_subject {
+    my $self = shift;
+    foreach my $tag (@{$self->tags}) {
+        next unless $tag =~ /^RT: (.+)/;
         return $1;
     }
 }
