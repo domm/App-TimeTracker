de9bb0b78abc26cecddac40ccdea1cdca2d9c59d
Thomas Klausner <domm@plix.at>
2011-08-02 11:17:00 +0200

    dist housekeeping

--- lib/App/TimeTracker.pm
+++ lib/App/TimeTracker.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use 5.010;
 
-our $VERSION = "2.007";
+our $VERSION = "2.008";
 # ABSTRACT: Track time spend on projects from the commandline
 
 use App::TimeTracker::Data::Task;
