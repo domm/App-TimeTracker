3b48ae158e11332af35df08e40d2d4b48947b6f9
Thomas Klausner <domm@plix.at>
2012-01-04 19:42:31 +0100

    prepare release 2.013

--- lib/App/TimeTracker.pm
+++ lib/App/TimeTracker.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use 5.010;
 
-our $VERSION = "2.012";
+our $VERSION = "2.013";
 # ABSTRACT: time tracking for impatient and lazy command line lovers
 
 use App::TimeTracker::Data::Task;
