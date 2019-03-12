7d5794a4fcf2f3a2b90a3bcbb782ef90a32f53b4
Thomas Klausner <domm@plix.at>
2019-03-12 11:01:52 +0100

    bump version

--- lib/App/TimeTracker.pm
+++ lib/App/TimeTracker.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use 5.010;
 
-our $VERSION = "2.025";
+our $VERSION = "2.026";
 # ABSTRACT: time tracking for impatient and lazy command line lovers
 
 use App::TimeTracker::Data::Task;
