9355b9fbde2f908d314fae7e45a4603ddea7482f
Thomas Klausner <domm@plix.at>
2020-12-28 20:53:42 +0100

    prepeare release

--- lib/App/TimeTracker.pm
+++ lib/App/TimeTracker.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use 5.010;
 
-our $VERSION = "3.006";
+our $VERSION = "3.007";
 # ABSTRACT: time tracking for impatient and lazy command line lovers
 
 use App::TimeTracker::Data::Task;
